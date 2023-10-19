66e1a232e1af54fd5f331ec25c1107ee7e6e66be
weisintai <wstai1214@gmail.com>
2023-10-19 22:54:36 +0800

    Update database schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,24 +1,32 @@
-import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  uuid,
+  text,
+  timestamp,
+  boolean,
+  pgEnum,
+} from "drizzle-orm/pg-core";
 
-export const User = pgTable("user", {
-    id: uuid("id").defaultRandom().primaryKey(),
-    username: text("username").notNull().unique(),
-    password: text("password").notNull(),
-    created_at: timestamp("created_at").defaultNow().notNull()
-});
+export const statusEnum = pgEnum("status", ["pending", "completed", "failed"]);
 
 export const Transaction = pgTable("transaction", {
-    id: uuid("id").defaultRandom().primaryKey(),
-    from: text("from").references( () => Account.account_number),
-    to: text("to").references( () => Account.account_number),
-    amount: text("amount").notNull(),
-    timestamp: timestamp("timestamp").defaultNow().notNull()
+  id: uuid("id").defaultRandom().primaryKey(),
+  from_account_id: text("from_account_id")
+    .references(() => Account.account_id)
+    .notNull(),
+  to_account_id: text("to_account_id")
+    .references(() => Account.account_id)
+    .notNull(),
+  amount: text("amount").notNull(),
+  timestamp: timestamp("timestamp").defaultNow().notNull(),
+  status: statusEnum("status").notNull(),
+  scheduled_at: timestamp("scheduled_at"), // null if not scheduled
 });
 
 export const Account = pgTable("account", {
-    account_number: text("account_number").primaryKey(),
-    id: uuid("id").references( () => User.id),
-    money: text("money").notNull(),
-    transfer_limit: text("transfer_limit").notNull(),
-    created_at: timestamp("created_at").defaultNow().notNull()
+  account_id: text("account_id").primaryKey(),
+  user_id: text("user_id").notNull(),
+  balance: text("balance").notNull(),
+  transfer_limit: text("transfer_limit").notNull(),
+  created_at: timestamp("created_at").defaultNow().notNull(),
 });
