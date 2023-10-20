10de54aee2f9cc5aa6c08c4373d63b0848677562
weisintai <wstai1214@gmail.com>
2023-10-21 00:57:58 +0800

    Restructure entire project to migrate from app directory to pages directory
    Pages dir is simple and fits my use case

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
+import { api } from "@/lib/api";
+
+import "../styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
