ef7e0df01fd08e51aa2b0b4ae43c559d7128f155
weisintai <wstai1214@gmail.com>
2023-10-21 13:11:50 +0800

    Add layout for navbar and to check if user has account

---
+++ src/components/layout.tsx
@@ -0,0 +1,37 @@
+import { MainNav } from "@/components/main-nav";
+import { useAccountStore } from "@/lib/store";
+import { UserButton } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const [hasAccount, setHasAccount] = useState(true);
+  const current_account_id = useAccountStore(
+    (state) => state.current_account_id
+  );
+
+  useEffect(() => {
+    setHasAccount(current_account_id !== null);
+  }, [current_account_id]);
+
+  return (
+    <main className={inter.className}>
+      {hasAccount ? (
+        <>
+          <div className="flex h-16 items-center px-4 border-b">
+            <MainNav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <UserButton afterSignOutUrl="/" />
+            </div>
+          </div>
+          {children}
+        </>
+      ) : (
+        <div>Create Account</div>
+      )}
+    </main>
+  );
+}
