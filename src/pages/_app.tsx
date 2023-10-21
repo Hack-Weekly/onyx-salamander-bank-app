4518205a486b3f90f379238bb66bffcba0e734a1
weisintai <wstai1214@gmail.com>
2023-10-21 13:14:22 +0800

    Add logic to chcck if user has account

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,37 @@
 import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "@/lib/api";
-import { Inter } from "next/font/google";
 
-// If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ["latin"] });
 import "../styles/globals.css";
+import { useAccountStore } from "@/lib/store";
+import { useEffect } from "react";
+import Layout from "@/components/layout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const accounts = api.account.list.useQuery();
+  const changeAccount = useAccountStore((state) => state.changeAccount);
+  const current_account_id = useAccountStore(
+    (state) => state.current_account_id
+  );
+
+  useEffect(() => {
+    // Check if the 'account' is null and if there is data in 'accounts.data'
+    if (
+      current_account_id === null &&
+      accounts.data &&
+      accounts.data.length > 0
+    ) {
+      // Set the first account from the 'accounts.data' list
+      // TODO: Maybe have option to set as default account
+      changeAccount(accounts.data[0].account_id);
+    }
+  }, [current_account_id, accounts, changeAccount]);
+
   return (
     <ClerkProvider {...pageProps}>
-      <main className={inter.className}>
+      <Layout>
         <Component {...pageProps} />
-      </main>
+      </Layout>
     </ClerkProvider>
   );
 };
