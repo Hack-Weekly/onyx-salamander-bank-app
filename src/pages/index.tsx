9d80085ed907705129eae0f142ffe6b863990e5a
weisintai <wstai1214@gmail.com>
2023-10-21 13:52:35 +0800

    Remove unused example code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,6 @@ const Home: NextPage = () => {
   const current_account_id = useAccountStore(
     (state) => state.current_account_id
   );
-  const hello = api.account.test.useQuery();
   const { isLoading, data: account } = api.account.getAccountDetail.useQuery({
     account_id: current_account_id!,
   });
@@ -30,42 +29,35 @@ const Home: NextPage = () => {
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
           {isLoading ? (
             "Loading..."
-          ) : account ? (
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">Balance</CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                </svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">${account.balance}</div>
-                <p className="text-xs text-muted-foreground">
-                  show growth from last month?
-                </p>
-              </CardContent>
-            </Card>
           ) : (
-            "No account"
+            <>
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">Balance</CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">${account!.balance}</div>
+                  <p className="text-xs text-muted-foreground">
+                    show growth from last month?
+                  </p>
+                </CardContent>
+              </Card>
+            </>
           )}
         </div>
       </div>
-
-      <p className="text-2xl ">
-        {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-      </p>
-      <Link href="/protected" className="text-xl">
-        Check out a protected procedure
-      </Link>
     </>
   );
 };
