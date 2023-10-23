21847b5b62cb3bae6ab242b3ddfffcd06e79c11a
weisintai <wstai1214@gmail.com>
2023-10-23 22:50:34 +0800

    Add more values to zustand store and make it persist in sessionStorage

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,13 +1,30 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface AccountState {
   current_account_id: string | null;
   changeAccount: (to: string | null) => void;
-  clearAccount: () => void;
+  hasAccount: boolean;
+  setHasAccount: (to: boolean) => void;
+  clearStore: () => void;
 }
 
-export const useAccountStore = create<AccountState>()((set) => ({
-  current_account_id: null,
-  changeAccount: (to) => set(() => ({ current_account_id: to })),
-  clearAccount: () => set(() => ({ current_account_id: null })),
-}));
+export const useAccountStore = create<AccountState>()(
+  persist(
+    (set, get) => ({
+      current_account_id: null,
+      changeAccount: (to) => set(() => ({ current_account_id: to })),
+      hasAccount: false,
+      setHasAccount: (to) => set(() => ({ hasAccount: to })),
+      clearStore: () => {
+        set(() => {
+          return { current_account_id: null, hasAccount: false };
+        });
+      },
+    }),
+    {
+      name: "account-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
