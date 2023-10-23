a9df47fb5cb147cd38f286b2db262030e9f5ed0a
weisintai <wstai1214@gmail.com>
2023-10-23 20:45:25 +0800

    Change component folder naming

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import { api } from "@/lib/api";
 import { useAccountStore } from "@/lib/store";
 import { UserButton } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
-import CreateAccount from "./create-account/create-account";
+import CreateAccount from "./account/create-account";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [hasAccount, setHasAccount] = useState(true);
