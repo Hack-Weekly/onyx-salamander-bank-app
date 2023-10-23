5a424dc7515ed4e4e1c12269c6040ded07d4da51
weisintai <wstai1214@gmail.com>
2023-10-23 20:28:56 +0800

    Allow changeAccount to take in null as parameter

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 interface AccountState {
   current_account_id: string | null;
-  changeAccount: (to: string) => void;
+  changeAccount: (to: string | null) => void;
 }
 
 export const useAccountStore = create<AccountState>()((set) => ({
