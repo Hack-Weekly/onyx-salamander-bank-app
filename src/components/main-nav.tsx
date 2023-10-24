4184e529fb2c892641c83709a7a865fd74b5724e
weisintai <wstai1214@gmail.com>
2023-10-24 15:09:27 +0800

    Move links to an array and map out to Link component

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,34 +1,36 @@
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import clsx from "clsx";
+
+const links = [
+  { name: "Overview", path: "/" },
+  { name: "Transfer", path: "/transfer" },
+  { name: "Settings", path: "/settings" },
+];
 
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  const pathname = usePathname();
+
   return (
     <nav
       className={cn("flex items-center space-x-4 lg:space-x-6", className)}
       {...props}
     >
-      <Link
-        href="/"
-        className="text-sm font-medium transition-colors hover:text-primary"
-      >
-        Overview
-      </Link>
-      <Link
-        href="/transfer"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Transfer
-      </Link>
-      <Link
-        href="/settings"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Settings
-      </Link>
+      {links.map((link) => (
+        <Link
+          href={link.path}
+          className={clsx(
+            "text-sm font-medium transition-colors hover:text-primary",
+            pathname !== link.path && "text-muted-foreground",
+          )}
+        >
+          {link.name}
+        </Link>
+      ))}
     </nav>
   );
 }
