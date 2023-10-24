f0717433f9a570b1fa99401a78023e4287d1bba6
lly02 <lly.02@outlook.com>
2023-10-24 08:04:04 +0800

    refactor: moved HEAD from index to layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import { type NextPage } from "next";
-import Head from "next/head";
 
 import { api } from "@/lib/api";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { useAccountStore } from "@/lib/store";
 import AccountCard from "@/components/account/account-card";
 import SkeletonAccountCard from "@/components/account/skeleton-account-card";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
   const { current_account_id } = useAccountStore();
@@ -19,14 +18,12 @@ const Home: NextPage = () => {
     }
   );
 
+  useEffect( () => {
+    document.title = "Dashboard | Onyx Salamaner Bank"
+  }, []);
+
   return (
     <>
-      <Head>
-        <title>Dashboard | Onyx Salamaner Bank</title>
-        <meta name="description" content="Hack Weekly" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <div className="flex-1 space-y-4 p-8 pt-6">
         <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
