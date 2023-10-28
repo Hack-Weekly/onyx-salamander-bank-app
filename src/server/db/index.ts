9e8c244487ddfe29c8d2633c8918c90f0d7106f3
weisintai <wstai1214@gmail.com>
2023-10-28 14:54:47 +0800

    Add schema to db object to allow for drizzle query

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,9 +3,10 @@ dotenv.config();
 
 import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
+import * as schema from "./schema";
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const db = drizzle(pool);
+const db = drizzle(pool, { schema });
 
 pool.connect();
 
