91f88681c005d5b5789956f6a25fa7225bc03ed2
weisintai <wstai1214@gmail.com>
2023-10-28 14:22:52 +0800

    feat: seperate account selection to show user's accounts and other accounts
    If user select own account and changed account to account that is selected, the transfer to field would be cleared

--- src/components/transfer/transfer-selector.tsx
+++ src/components/transfer/transfer-selector.tsx
@@ -1,5 +1,5 @@
 import { PopoverTrigger } from "@radix-ui/react-popover";
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "../ui/button";
 import { Popover, PopoverContent } from "../ui/popover";
 import {
@@ -13,19 +13,31 @@ import {
 import { api } from "@/lib/api";
 import { CaretSortIcon } from "@radix-ui/react-icons";
 import { useAccountStore } from "@/lib/store";
+import { useUser } from "@clerk/nextjs";
 
 export default function TransferSelector({
-  type,
   account_id,
   onChange,
 }: {
-  type: string;
   account_id: string;
   onChange: (account_id: string) => void;
 }) {
+  const { user } = useUser();
   const { current_account_id } = useAccountStore();
   const [open, setOpen] = React.useState(false);
   const { data: accounts } = api.account.allAccounts.useQuery();
+  const current_user_accounts = accounts
+    ? accounts.filter((account) => account.user_id === user?.id)
+    : [];
+  const other_accounts = accounts
+    ? accounts.filter((account) => account.user_id !== user?.id)
+    : [];
+
+  useEffect(() => {
+    if (account_id === current_account_id) {
+      onChange("");
+    }
+  }, [account_id, current_account_id]);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -34,11 +46,11 @@ export default function TransferSelector({
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          aria-label="To account"
+          aria-label="Select account"
           className="w-max justify-between"
         >
           <span className="inline-block w-full overflow-hidden overflow-ellipsis">
-            {account_id == "" ? `${type} account` : account_id}
+            {account_id == "" ? `Select account` : account_id}
           </span>
           <CaretSortIcon className="ml-auto h-4 w-4 shrink-0 opacity-50" />
         </Button>
@@ -48,8 +60,26 @@ export default function TransferSelector({
           <CommandList>
             <CommandInput placeholder="Search account..." />
             <CommandEmpty>No account found.</CommandEmpty>
+            {current_user_accounts.length > 0 && (
+              <CommandGroup heading="Your Accounts">
+                {current_user_accounts.map((account) =>
+                  account.account_id == current_account_id ? null : (
+                    <CommandItem
+                      key={account.account_id}
+                      onSelect={(id) => {
+                        setOpen(false);
+                        onChange(id);
+                      }}
+                      className="text-sm"
+                    >
+                      {account.account_id}
+                    </CommandItem>
+                  ),
+                )}
+              </CommandGroup>
+            )}
             <CommandGroup heading="Account">
-              {accounts?.map((account) =>
+              {other_accounts?.map((account) =>
                 account.account_id == current_account_id ? null : (
                   <CommandItem
                     key={account.account_id}
