e1150d6e3930916013af5ea1d34a3f0f5c6e8620
weisintai <wstai1214@gmail.com>
2023-10-28 14:23:10 +0800

    Improve transfer page styling and form schema

--- src/pages/transfer.tsx
+++ src/pages/transfer.tsx
@@ -6,6 +6,7 @@ import {
   FormControl,
   FormItem,
   FormMessage,
+  FormLabel,
 } from "@/components/ui/form";
 import { api } from "@/lib/api";
 import { useForm } from "react-hook-form";
@@ -14,15 +15,20 @@ import * as z from "zod";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
 import { Loader2 } from "lucide-react";
+import Head from "next/head";
+import { Separator } from "@/components/ui/separator";
 
 const formSchema = z
   .object({
     transfer_to: z.string().min(1, {
-      message: "Indicate bank account to transfer to.",
+      message: "Please indicate bank account to transfer to.",
     }),
-    amount: z.string().min(1, {
-      message: "Indicate amount to transfer.",
-    }),
+    amount: z.string().refine(
+      (value) => {
+        return /^\d*\.?\d+$/.test(value) && parseFloat(value) > 0;
+      },
+      { message: "Plase enter a positive number." },
+    ),
   })
   .required();
 
@@ -52,47 +58,52 @@ export default function Transfer() {
 
   return (
     <>
+      <Head>
+        <title>Transfer | Onyx Salamaner Bank</title>
+      </Head>
+      <h2 className="text-3xl font-bold tracking-tight">Transfer</h2>
+      <p className="text-muted-foreground">Send money between accounts.</p>
+      <Separator className="my-6" />
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="flex flex-col items-center justify-center space-y-2">
-            <FormField
-              control={form.control}
-              name="transfer_to"
-              render={({ field: { onChange, value } }) => (
-                <FormItem>
-                  <FormControl>
-                    <TransferSelector
-                      type="To"
-                      account_id={value}
-                      onChange={onChange}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="transfer_to"
+            render={({ field: { onChange, value } }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Transfer to</FormLabel>
+                <FormControl>
+                  <TransferSelector account_id={value} onChange={onChange} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            <FormField
-              control={form.control}
-              name="amount"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder="Amount" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+          <FormField
+            control={form.control}
+            name="amount"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Amount</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter an amount" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            <Button type="submit" disabled={form.formState.isSubmitting}>
-              {form.formState.isSubmitting ? (
+          <Button type="submit" disabled={form.formState.isSubmitting}>
+            {form.formState.isSubmitting ? (
+              <span className="flex items-center gap-1">
                 <Loader2 className="h-6 w-6 animate-spin" />
-              ) : (
-                "Transfer now"
-              )}
-            </Button>
-          </div>
+                Transferring
+              </span>
+            ) : (
+              "Transfer now"
+            )}
+          </Button>
         </form>
       </Form>
     </>
