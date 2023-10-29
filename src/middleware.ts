f3289e57507f8321d9a1937f31867cc11bfaa3d5
weisintai <wstai1214@gmail.com>
2023-10-29 12:46:17 +0800

    Make /api/cookie public so we can clear account_id cookie when signed out

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/sign-out"],
+  publicRoutes: ["/sign-out", "/api/cookie"],
 });
 
 export const config = {
