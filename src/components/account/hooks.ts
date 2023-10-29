26234520ff5e6639c73c48a47a81664edd3721cd
weisintai <wstai1214@gmail.com>
2023-10-29 12:51:16 +0800

    Enhance createAccount hook to match current features of system

--- src/components/account/hooks.ts
+++ src/components/account/hooks.ts
@@ -1,12 +1,25 @@
 import { api } from "@/lib/api";
+import { useAccountStore } from "@/lib/store";
 
 export const useCreateAccount = () => {
   const accountCreation = api.account.createAccount.useMutation();
   const utils = api.useUtils();
+  const { changeAccount } = useAccountStore();
 
   const createAccount = async () => {
-    await accountCreation.mutateAsync();
-    utils.account.invalidate();
+    const result = await accountCreation.mutateAsync();
+    fetch("/api/cookie", {
+      method: "POST",
+      body: JSON.stringify({
+        name: "current_account_id",
+        value: result.account_id,
+      }),
+    }).then(() => {
+      changeAccount(result.account_id);
+      utils.account.getAccountDetail.reset();
+      utils.account.getPreferences.reset();
+      utils.transaction.getTransactionsHistory.reset();
+    });
   };
 
   return createAccount;
